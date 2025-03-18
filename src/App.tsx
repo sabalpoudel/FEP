3072b4818e2899b623c28fda931124fc79de5f10
sabal poudel <sabalenfiniy@gmail.com>
2025-03-18 08:26:04 +0545

    initial react set up using vite ts and mui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+export default function MyApp() {
+  return (
+    <React.Fragment>
+      <CssBaseline enableColorScheme />
+      {/* The rest of your application */}
+    </React.Fragment>
+  );
+}
