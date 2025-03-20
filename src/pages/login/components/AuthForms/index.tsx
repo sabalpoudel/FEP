57bdb535fd79956b90ab3fbbd4fca849ca9772c2
sabal poudel <sabalenfiniy@gmail.com>
2025-03-20 20:52:29 +0545

    feat: added url toglging for sign-in sign-up

--- src/pages/login/components/AuthForms/index.tsx
+++ src/pages/login/components/AuthForms/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 import { AuthFormsWrapper } from "./AuthFormsWrapper";
+import { Link, useSearchParams } from "react-router-dom";
 
 type TAuthForms = { className?: string };
 
+type TAuthType = "signin" | "signup";
 const AuthForms: React.FC<TAuthForms> = (props) => {
   const {} = props;
+
   const { t } = useTranslation();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryType = searchParams.get("type");
+  const type: TAuthType = queryType === "signup" ? "signup" : "signin";
+  const isSignIn = type === "signin";
+
+  const toggleAuthType = () => {
+    setSearchParams({ type: isSignIn ? "signup" : "signin" });
+  };
 
   return (
     <AuthFormsWrapper className="afw">
@@ -31,23 +43,27 @@ const AuthForms: React.FC<TAuthForms> = (props) => {
             className="afw-wrapper-title-sub"
           >
             <Typography color="textSecondary">
-              {t("already_have_an_account")}
+              {t(
+                isSignIn ? "do_not_have_an_account" : "already_have_an_account"
+              )}
             </Typography>
-            <Typography component="u" color="primary">
-              {t("sign_in")}
-            </Typography>
+            <Button color="primary" onClick={toggleAuthType}>
+              <u>{t(isSignIn ? "sign_up" : "sign_in")}</u>
+            </Button>
           </Typography>
         </div>
         <div className="afw-wrapper-form"></div>
         <div className="afw-wrapper-footer">
           <Typography
             variant="body2"
-            color="textSecondary"
             component="div"
+            color="textSecondary"
             className="afw-wrapper-footer-texts"
           >
             <Typography>{t("by_signing_up_you_agree_to_our")}</Typography>
-            <Typography component="u">{t("privacy_policy")}</Typography>
+            <Typography component={Link} to="/privacy" color="textSecondary">
+              <u>{t("privacy_policy")}</u>
+            </Typography>
           </Typography>
         </div>
       </div>
