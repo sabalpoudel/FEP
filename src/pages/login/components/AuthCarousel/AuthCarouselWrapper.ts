9221ffa36e26de7bd66a20f08744d667ca74868b
sabal poudel <sabalenfiniy@gmail.com>
2025-03-21 18:03:49 +0545

    add: added swiper carousel at login page

--- src/pages/login/components/AuthCarousel/AuthCarouselWrapper.ts
+++ src/pages/login/components/AuthCarousel/AuthCarouselWrapper.ts
@@ -11,6 +11,34 @@ const AuthCarouselWrapper = styled.div<{ theme?: Theme }>`
   ${breakpoint("ssm")} {
     display: none;
   }
+  .afw {
+    &-wrapper {
+      width: 100%;
+      height: 100%;
+      &-swiper {
+        width: 100%;
+        height: 100%;
+        &-slide {
+          width: 100%;
+          height: 100%;
+          position: relative;
+          &-img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+          &-text {
+            left: 0;
+            right: 0;
+            color: #fff;
+            bottom: 2rem;
+            padding: 1rem;
+            position: absolute;
+          }
+        }
+      }
+    }
+  }
 `;
 
 export { AuthCarouselWrapper };
--- src/pages/login/components/AuthCarousel/index.tsx
+++ src/pages/login/components/AuthCarousel/index.tsx
@@ -1,13 +1,61 @@
 import React from "react";
 import { AuthCarouselWrapper } from "./AuthCarouselWrapper";
+import { CustomSwiper } from "../../../../components/bits/Swiper/SwiperComp";
+import { Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 type TAuthCarousel = { className?: string };
 
+const slides = [
+  {
+    img_src: "/assets/images/banner/banner.webp",
+    text_key: "celebrate_earth_day_with_us",
+  },
+  {
+    img_src: "/assets/images/banner/banner.webp",
+    text_key: "celebrate_earth_day_with_us",
+  },
+  {
+    text_key: "celebrate_earth_day_with_us",
+    img_src: "/assets/images/banner/banner.webp",
+  },
+];
+
 const AuthCarousel: React.FC<TAuthCarousel> = (props) => {
   const {} = props;
+  const { t } = useTranslation();
+
+  const Slides = slides.map(({ text_key, img_src }, j: number) => {
+    return (
+      <div key={text_key} className="afw-wrapper-swiper-slide">
+        <img
+          src={img_src}
+          alt={text_key}
+          className="afw-wrapper-swiper-slide-img"
+        />
+        <Typography
+          variant="h4"
+          align="center"
+          className="afw-wrapper-swiper-slide-text"
+        >
+          {t(`Banner.${text_key}`)}&nbsp;{j}
+        </Typography>
+      </div>
+    );
+  });
+
   return (
     <AuthCarouselWrapper className="afw">
-      <div className="afw-wrapper"></div>
+      <div className="afw-wrapper">
+        <CustomSwiper
+          delay={5000}
+          slides={Slides}
+          autoplay={true}
+          slidesPerView={1}
+          pagination={true}
+          className="afw-wrapper-swiper"
+        />
+      </div>
     </AuthCarouselWrapper>
   );
 };
